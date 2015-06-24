48c30175031005fe99bd9f8ef6cca0dfce6a443a
Edward4j <eduard.dovgal@gmail.com>
2015-06-25 02:17:37 +0300

    Correcting create answer test

--- spec/features/create_answer_spec.rb
+++ spec/features/create_answer_spec.rb
@@ -20,7 +20,7 @@ feature 'Create Answer', %q{
     expect(page).to have_content(question.title)
     expect(page).to have_content(question.body)
     question.answers.each do |qa|
-      expect(page).to have_content(qa.body)
+      expect(page).to have_content(qa.body) if qa == question.answers.last
     end
     expect(current_path).to eq(question_path(question))
   end
