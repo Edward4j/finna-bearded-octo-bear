11eb40533d4c7e477549d2f6e698d48fa9f1743f
Edward4j <eduard.dovgal@gmail.com>
2015-07-10 15:31:20 +0300

    Question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,7 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :attachments
 
+  accepts_nested_attributes_for :attachments
+
   validates :title, :body, :user_id, presence: true
 end
